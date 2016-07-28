63099fa31dac40e832cbf8d3321fb833731e9423
isophotek <isophotek@gmail.com>
2016-07-28 23:45:32 +0200

    added ap, pe stream

--- .idea/krpc.py
+++ .idea/krpc.py
@@ -2,3 +2,8 @@
 
 import krpc
 
+conn = krpc.connect(name='Example')
+
+apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+periapsis = conn.add_stream(getattr, vessel.orbit, 'periapsis_altitude')
+
