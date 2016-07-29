4d2c239b2b16e1d9a587f023d7b861c44ca53d20
isophoptek <isophotek@gmail.com>
2016-07-29 14:06:19 +0200

    basic functionality, add deltav data

--- DataGrab.py
+++ DataGrab.py
@@ -8,14 +8,14 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("outfile", help="Output filename")
+parser.add_argument("path", help="Path fox export")
 parser.add_argument("-n", "--polls", help="Number of polls", type=int, default=1)
 args = parser.parse_args()
 
 # argument interpretation
 
 num_of_polls = args.polls
-outFile = args.outfile
+outFile = args.path
 
 # connect to server
 
@@ -24,8 +24,6 @@ conn = krpc.connect(name='DataGrab')
 # create data streams
 
 vessel = conn.space_center.active_vessel
-refframe = vessel.orbit.body.reference_frame
-position = conn.add_stream(vessel.position, refframe)
 apoapsis = conn.add_stream(vessel.orbit.apoapsis_altitude)
 periapsis = conn.add_stream(vessel.orbit.periapsis_altitude)
 currentbody = conn.add_stream(vessel.orbit.body)
@@ -35,13 +33,61 @@ currentgforce = conn.add_stream(vessel.flight.g_force)
 meanaltitude = conn.add_stream(vessel.flight.mean_altitude)
 terminalvelocity = conn.add_stream(vessel.flight.terminal_velocity)
 
+# export file definition
+
+if num_of_polls == 1:
+    fileName = "SinglePoll_" + str(vessel.name) + "_" + str(vessel.situation) + "_" + str(missionelapsedtime) + ".csv"
+else:
+    fileName = "PollLog_" + str(vessel.name) + str(missionelapsedtime) + ".csv"
+
+outFile += fileName
+
 # poll loop
 
 poll = 0
+with open(outFile) as exportFile:
+    exportFile.write("UT",
+                     "MET",
+                     "Current Body",
+                     "Apoapsis",
+                     "Periapsis",
+                     "Inclination",
+                     "MeanAltitude",
+                     "G Force",
+                     "Terminal Velocity",
+                     "Stage dV",
+                     "Total dV",
+                     "/n")
 
 while poll < num_of_polls:
     # TODO get data values
-    # TODO write to file
+    line = ("{ut},"
+            "{met},"
+            "{body},"
+            "{apo},"
+            "{peri},"
+            "{inc},"
+            "{mean_alt},"
+            "{gforce},"
+            "{vt},"
+            "{stagedv},"
+            "{totaldv},"
+            "/n").format(ut="ut",
+                         met=meanaltitude,
+                         body=currentbody,
+                         apo=apoapsis,
+                         peri=periapsis,
+                         inc=inclination,
+                         mean_alt=meanaltitude,
+                         gforce=currentgforce,
+                         vt=terminalvelocity,
+                         stagedv="",
+                         totaldv="",)
+#TODO get deltaV data
+
+# TODO write to file
+    exportFile.write(line)
+
     poll += 1
 
 print("Cycle done")
