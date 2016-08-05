476806a62fb4cb6f608563ea25d22881d726dbaa
Admin <isophotek@gmail.com>
2016-08-05 10:42:33 +0200

    hover.py commit

---
+++ hover.py
@@ -0,0 +1,25 @@
+import krpc
+import time
+
+conn = krpc.connect()
+vessel = conn.space_center.active_vessel
+control = vessel.control
+flight = vessel.flight(vessel.orbit.body.reference_frame)
+
+target = 15 # target altitude above the surface, in meters
+g = 9.81
+while True:
+
+    alt_error = target - flight.surface_altitude
+
+    # compute the desired acceleration:
+    #   g   to counteract gravity
+    #   -v  to push the vertical speed towards 0
+    #   e   to push the altitude error towards 0
+    a = g - flight.vertical_speed + alt_error
+
+    # Compute throttle setting using newton's law F=ma
+    F = vessel.mass * a
+    control.throttle = F / vessel.available_thrust
+
+time.sleep(0.01)
