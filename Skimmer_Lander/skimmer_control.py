3d8dae0c638526288eb6c676fc598ddddbd4417f
isophoptek <isophotek@gmail.com>
2016-08-12 11:12:55 +0200

    skimmer control update, ready for debug testing

--- Skimmer_Lander/skimmer_control.py
+++ Skimmer_Lander/skimmer_control.py
@@ -20,8 +20,30 @@ def ascend_to_ap(target_ap, target_heading):
     ap = vessel.auto_pilot
     ap.reference_frame = vessel.orbital_reference_frame
 
-    ap.target_pitch = 90
+    ap.target_pitch = 0
+    ap.target_heading = target_heading
     ap.engage()
-    vessel.control.throttle = 1
 
     while meanaltitude < ground_clearance_altitude:
+        vessel.control.throttle = 1
+        if time_to_ap < 40 and vessel.control.throttle != 1:
+            vessel.control.throttle += 0.01
+        elif time_to_ap > 40:
+            vessel.control.throttle -= 0.01
+
+    while meanaltitude < ap_vector_clearance_altitude:
+        ap.target_pitch = 45
+        if time_to_ap < 40 and vessel.control.throttle != 1:
+            vessel.control.throttle += 0.01
+        elif time_to_ap > 40:
+            vessel.control.throttle -= 0.01
+
+    while apoapsis < target_ap:
+        ap.target_direction = (0, 1, 0)
+        if time_to_ap < 40 and vessel.control.throttle != 1:
+            vessel.control.throttle += 0.01
+        elif time_to_ap > 40:
+            vessel.control.throttle -= 0.01
+
+    ap.disengage()
+    vessel.auto_pilot.sas = True
