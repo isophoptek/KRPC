26feb421dc03a79fed644842c7d5e29d94d7591b
isophoptek <isophotek@gmail.com>
2017-02-09 17:19:02 +0100

    hodur procedures, engine selection

--- SC2017/Baldur/Hodur/hodur_procedures.py
+++ SC2017/Baldur/Hodur/hodur_procedures.py
@@ -3,25 +3,91 @@
 
 def deorbit():
 
-import krpc
-from time import sleep
+    import krpc
+    from time import sleep
 
 # Connection
 
-try:
-    print('Connecting to server as: Emergency Deorbit')
-    conn = krpc.connect(name='Emergency Deorbit')
-except krpc.error.NetworkError:
-    print('Connection to server could not be established.')
-    print('Check if server is running and accepts connections, accept connection manually if necessary.')
-    exit(1)
+    try:
+        print('Connecting to server as: Emergency Deorbit')
+        conn = krpc.connect(name='Emergency Deorbit')
+    except krpc.error.NetworkError:
+        print('Connection to server could not be established.')
+        print('Check if server is running and accepts connections, accept connection manually if necessary.')
+        exit(1)
 
-# turn to retrograde
+    vessel = conn.space_center.active_vessel
+    control = vessel.control
+    ref_frame = vessel.surface_reference_frame
+    flight = vessel.flight
+    orbit = vessel.orbit
+    ap = vessel.auto_pilot
+    ap.reference_frame = vessel.orbital_reference_frame
+    apoapsis = conn.add_stream(getattr, orbit, 'apoapsis_altitude')
+    periapsis = conn.add_stream(getattr, orbit, 'periapsis_altitude')
 
-# try main engine
+    # turn to retrograde
 
-# try backup engine
+    ap.sas_mode = 'retrograde'
+    ap.engage()
 
-# try emergency RCS
+    countdown = 15
+    while countdown > 0:
+        print countdown
+        sleep(1)
+    # try main engine
+    # search for and activate main engine, deactivate other
+    part_list = vessel.parts.with_tag('Main_Engine')
+    for item in part_list:
+        item.active = True
+    part_list = vessel.parts.with_tag('Backup_Engine')
+    for item in part_list:
+        item.active = False
+    part_list = vessel.parts.with_tag('Emergency_Engine')
+    for item in part_list:
+        item.active = False
 
-# burn until ap or pe less then 30km
+    if vessel.available_thrust != 0:
+        # activate main engine
+        while apoapsis > 30000 or periapsis > 30000:
+            control.throttle = 1
+        control.throttle = 0
+    else: # try backup engine
+        # search for and activate backup engines, deactivate other
+        part_list = vessel.parts.with_tag('Main_Engine')
+        for item in part_list:
+            item.active = False
+        part_list = vessel.parts.with_tag('Backup_Engine')
+        for item in part_list:
+            item.active = True
+        part_list = vessel.parts.with_tag('Emergency_Engine')
+        for item in part_list:
+            item.active = False
+
+        if vessel.available_thrust != 0:
+            # activate backup engine
+            while apoapsis > 30000 or periapsis > 30000:
+                control.throttle = 1
+            control.throttle = 0
+        else: # try emergency RCS
+            # search for and activate emergency RCS engines, deactivate other
+            part_list = vessel.parts.with_tag('Main_Engine')
+            for item in part_list:
+                item.active = False
+            part_list = vessel.parts.with_tag('Backup_Engine')
+            for item in part_list:
+                item.active = False
+            part_list = vessel.parts.with_tag('Emergency_Engine')
+            for item in part_list:
+                item.active = True
+
+            if vessel.available_thrust != 0:
+                # activate emergency RCS engine
+                while apoapsis > 30000 or periapsis > 30000:
+                    control.throttle = 1
+                control.throttle = 0
+    # burn until ap or pe less then 30km
+
+    # decouple capsule, wait, separation burn (retrograde)
+
+
